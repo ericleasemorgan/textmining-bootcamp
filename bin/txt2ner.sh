45938b77b39a98aeb2f79a48c87f5281468379b9
Eric Lease Morgan <eric_morgan@infomotions.com>
2018-05-13 11:40:11 -0400

    'Finished' building a corpus

--- bin/txt2ner.sh
+++ bin/txt2ner.sh
@@ -9,6 +9,8 @@
 # configure
 NERCLIENT='./bin/ner-client.pl'
 NER2TAB='./bin/ner2tab.pl'
+TMP='./tmp'
+HOME='/afs/crc.nd.edu/user/e/emorgan/local/english'
 
 # get input
 FILE=$1
@@ -16,21 +18,22 @@ FILE=$1
 # do error checking here
 
 # initialize
+cd $HOME
 BASENAME=$( basename $FILE .txt )
 DIRNAME=$( dirname $FILE )
-XML="$DIRNAME/$BASENAME.xml"
-TSV="$DIRNAME/$BASENAME.tsv"
+XML="$TMP/$BASENAME.xml"
+NER="$DIRNAME/$BASENAME.ner"
 
 # debug
 echo $FILE >&2
 echo $XML  >&2
-echo $TSV  >&2
+echo $NER  >&2
 echo       >&2
 
 # do the work
 $NERCLIENT $FILE > $XML
-$NER2TAB $XML    > $TSV
-rm $FILE
+$NER2TAB $XML    > $NER
+rm $XML
 
 # done
 exit
